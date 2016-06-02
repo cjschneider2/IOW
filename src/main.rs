bbbf50c7fd0a03f6526400af5f1a00bfddedb19e
Charles J. Schneider <cjschneider2@gmail.com>
2016-06-02 08:17:43 +0200

    Implemnt basic functionality of searching files for terms

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use std::io::prelude::*;
+use std::io;
 use std::env;
 use std::path::PathBuf;
+use std::fs::File;
 
 fn main() {
+
+    // Read the first command line argument
+    let mut argv = env::args();
+    let needle = match argv.nth(1) {
+        None => {
+            println!("No search argument given\nUsage: `iow wtf`");
+            return;
+        }
+        Some(arg) => arg,
+    };
 
     // Setting the search path for the dictionaries
     let iow_dir = {
@@ -16,10 +29,27 @@ fn main() {
     };
     println!("Looking for dictionaries in: {:?}", iow_dir);
 
-    // TODO: Dictionary finding code
+    let mut entry_found = false;
 
-    // TODO: Search the dictionaries for the term
+    let dir = iow_dir.read_dir().unwrap();
+    for file in dir.filter_map(|f| f.ok()) {
+        let file = File::open(file.path()).ok().unwrap();
+        let reader = io::BufReader::new(file);
+        for line in reader.lines().filter_map(|l| l.ok()) {
+            let mut words: Vec<&str> = line.splitn(2, ' ').collect();
+            let (a, d) = match (words.pop(), words.pop()) {
+                (Some(d), Some(a)) => (a, d),
+                (_, _) => ("", ""),
+            };
 
-    // TODO: Print out the found term (if any)
+            if a.to_uppercase() == needle.to_uppercase() {
+                println!("{}: {}", a, d.trim());
+                entry_found = true;
+            }
+        }
+    }
 
+    if !entry_found {
+        println!("Definition for {} not found.", needle);
+    }
 }
